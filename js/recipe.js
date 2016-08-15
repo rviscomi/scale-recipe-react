d25be99a0afd5e32c2d31ce9d209cc85dd699a51
Rick Viscomi <rviscomi@gmail.com>
2016-08-14 17:18:17 -0700

    Basic recipe Still needs to update ingredients on serving size change

--- js/recipe.js
+++ js/recipe.js
@@ -1,7 +1,79 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const RECIPES = [{
+  name: 'Pasta Dough',
+  serves: 4,
+  ingredients: [
+    ['4', 'eggs'],
+    ['4', 'cups', 'flour'],
+    ['1/4', 'cup', 'olive oil'],
+    ['1', 'tablespoon', 'salt']
+  ]
+}];
+
+const Recipes = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {RECIPES.map(recipe => <Recipe key={recipe.name} {...recipe}/>)}
+      </div>
+    );
+  }
+});
+
+const Recipe = React.createClass({
+  getInitialState: function() {
+    return {
+      serves: this.props.serves,
+      ingredients: this.props.ingredients
+    };
+  },
+  onChange: function(e) {
+    this.setState({
+      serves: e.target.value
+    });
+  },
+  render: function() {
+    return (
+      <section>
+        <h1>
+          {this.props.name}
+          <input
+            type="number" min="0"
+            value={this.state.serves}
+            step={this.props.serves/4}
+            onChange={this.onChange} />
+        </h1>
+        <Ingredients ingredients={this.state.ingredients} />
+      </section>
+    );
+  }
+});
+
+const Ingredients = React.createClass({
+  render: function() {
+    return (
+      <ul>
+        {this.props.ingredients.map(([quantity, unit, item]) =>
+          <Ingredient
+            key={`${quantity} ${unit} ${item}`}
+            quantity={quantity} unit={unit} item={item} />
+        )}
+      </ul>
+    );
+  }
+});
+
+const Ingredient = React.createClass({
+  render: function() {
+    return(
+      <li>{this.props.quantity} {this.props.unit} {this.props.item}</li>
+    );
+  }
+});
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
+  <Recipes/>,
+  document.getElementById('recipes')
 );
